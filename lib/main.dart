7d94fe277fd28fdf67bcb44b521f8a91f5aacc3d
vladimir <vladimir.hodaryonok>
2022-10-21 11:19:01 +0300

    feat: on_pr_pipeline #1

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:presentation/presentation.dart';
 import 'package:star_movie/di/init_dependencies.dart';
 import 'package:star_movie/flavor_configurator/flavor_configurator.dart';
-import 'firebase_options.dart';
 
 Future<void> initApp(Flavor flavor) async {
   WidgetsFlutterBinding.ensureInitialized();
   final flavorConfigurator = FlavorConfigurator(flavor: flavor);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   await FirebaseCrashlytics.instance
       .setCrashlyticsCollectionEnabled(kReleaseMode);
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
