8154b585bc1a12432f9ed59ec7d8de78f049eb7c
vladimir <vladimir.hodaryonok>
2022-11-02 09:17:49 +0300

    fix: comments #2 (hot)

--- packages/presentation/lib/style/colors.dart
+++ packages/presentation/lib/style/colors.dart
@@ -2,11 +2,8 @@ class MyColors {
   const MyColors._();
 
   static const int splashScreenUp = 0xffE51937;
-
   static const int splashScreenBottom = 0xffDB5252;
-
   static const int messageBackground = 0xff2B3543;
-
   static const int colorTextField = 0xff2B3543FF;
   static const int colorFaceBook = 0xff3B5A9AFF;
   static const int colorGoogle = 0xffCB3E2DFF;
